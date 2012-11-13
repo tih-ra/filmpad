847ebf5dfb727af7f802cfa7ac622c239cc16085
Andriy Bazyuta <andriy.bazyuta@gmail.com>
2012-11-13 20:09:05 +0200

    new bucket

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,5 +4,5 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => 'AKIAIVIEJMDMHRSA7QDA',
     :aws_secret_access_key  => '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'
   }
-  config.fog_directory  = 'aditapmedia'
+  config.fog_directory  = 'aditapmediacache'
 end
